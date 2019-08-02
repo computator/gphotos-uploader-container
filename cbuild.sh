ae02ec636f0c0500d4decc2bc87972beacce4d0d
Reuben Lifshay <rlifshay@gmail.com>
2019-08-01 23:32:14 -0700

    Partially working oauth proxy.

--- cbuild.sh
+++ cbuild.sh
@@ -3,55 +3,41 @@ set -e
 
 ctr=$(buildah from wine:3.0)
 
-buildah config --env WINEPREFIX=/opt/gphotos-uploader $ctr
+WINEPREFIX=/opt/gphotos-uploader
+buildah config \
+	--env WINEPREFIX=$WINEPREFIX \
+	--env WINEDEBUG=-all \
+	$ctr
 
 buildah add $ctr "https://dl.google.com/drive/gsync_enterprise64.msi" /tmp/
-buildah add $ctr "http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86_64.msi" /tmp/
+buildah copy $ctr user_setup.config /config/user_default/
 buildah run $ctr sh -c '
 	mkdir -p "$WINEPREFIX/drive_c/users/root/Local Settings/Application Data/Google"
-	ln -s /config "$WINEPREFIX/drive_c/users/root/Local Settings/Application Data/Google/Drive"
-	mkdir -p /config/user_default
-	cat > /config/user_default/user_setup.config' <<-"E_GSYNC_CONFIG"
-		[Computers]
-		desktop_enabled: False
-		documents_enabled: False
-		pictures_enabled: False
-		folders: /upload
-		# if high quality is disabled it means upload in original quality
-		high_quality_enabled: False
-		always_show_in_photos: True
-		usb_sync_enabled: False
-		ignore_extensions: ext1, ext2, ext3
-		# Delete mode can be: ALWAYS_SYNC_DELETES, ASK, NEVER_SYNC_DELETES
-		delete_mode: NEVER_SYNC_DELETES
-
-		[MyDrive]
-		my_drive_enabled: False
-		# folder: /path/to/google_drive
-
-		[Settings]
-		autolaunch: True
-		show_overlays: False
-
-		[Network]
-		# download_bandwidth: 100
-		# upload_bandwidth: 200
-		# use_direct_connection: False
-	E_GSYNC_CONFIG
+	ln -s /config "$WINEPREFIX/drive_c/users/root/Local Settings/Application Data/Google/Drive"'
 buildah run $ctr sh -c '
-	wine64 msiexec /i /tmp/wine_gecko-2.47-x86_64.msi || exit $?
 	wine64 msiexec /i /tmp/gsync_enterprise64.msi || exit $?
+	# terminate wineserver etc to allow everything to save state
 	while pkill -P 1; do
 		sleep 0.1
 	done'
-buildah run $ctr rm -f /tmp/gsync_enterprise64.msi /tmp/wine_gecko-2.47-x86_64.msi
+buildah run $ctr rm -f /tmp/gsync_enterprise64.msi # /tmp/wine_gecko-2.47-x86_64.msi
+
+buildah copy $ctr oauth-proxy-url-handler.sh /usr/local/bin/oauth-proxy-url-handler
+buildah run $ctr sed -i \
+	-e '/\[Software\\\\Classes\\\\http\(s\)\?\\\\shell\\\\open\\\\command\]/,/^$/ s/winebrowser\.exe\\" -nohome"/winebrowser.exe\\" \\"%1\\""/' \
+	-e '/\[Software\\\\Classes\\\\http\(s\)\?\\\\shell\\\\open\\\\ddeexec/,/^$/ d' \
+	"$WINEPREFIX/system.reg"
+buildah run $ctr sh -c 'cat >> "$WINEPREFIX/user.reg"' <<-"E_REGTEXT"
+	[Software\\Wine\\WineBrowser] 1564726662
+	#time=1d548f9fe2e6018
+	"Browsers"="oauth-proxy-url-handler"
+E_REGTEXT
 
 buildah config \
-	--cmd 'wine64 "/opt/gphotos-uploader/drive_c/Program Files/Google/Drive/googledrivesync.exe"' \
+	--cmd "wine64 '$WINEPREFIX/drive_c/Program Files/Google/Drive/googledrivesync.exe'" \
 	--workingdir /config \
 	--volume /config \
 	--volume /upload \
-	--env WINEDEBUG=-all \
 	$ctr
 
 img=$(buildah commit --rm $ctr gphotos-uploader)
