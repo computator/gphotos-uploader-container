e0f6539667ae501a9d06e34f82c0eaf830e0b663
Reuben Lifshay <rlifshay@gmail.com>
2019-08-09 22:27:05 -0700

    Make logging work.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 set -e
 
+pid=
+
 handle_exit() {
-	pkill -P $$
+	# kill children of pid if set, otherwise all children
+	pkill -P ${!:-$$}
 }
 
 if [ "$1" = 'googledrivesync' ]; then
 	trap handle_exit INT TERM
 	wine64 "$WINEPREFIX/drive_c/Program Files/Google/Drive/googledrivesync.exe" &
-	wait $!
+	pid=$!
+	tail --follow=name --retry --lines=0 --pid $pid /config/user_default/sync_log.log 2> /dev/null &
+	wait $pid
 	exit $?
 fi
 
